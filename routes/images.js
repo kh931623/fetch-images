c54740cce1caa3a24384445e0c93018d3aa3c95f
Simon <kh931623@hotmail.com>
2024-03-09 19:13:26 +0800

    feat: format unsplash images

--- routes/images.js
+++ routes/images.js
@@ -3,14 +3,13 @@ var router = express.Router();
 
 const { searchImages } = require('../services/unsplash')
 
+
 /* GET users listing. */
 router.get('/', async function (req, res, next) {
   const key = req.query.key
   const images = await searchImages(key)
 
-  res.json({
-    images
-  })
+  res.json(images)
 });
 
 module.exports = router;
--- services/unsplash.js
+++ services/unsplash.js
@@ -1,4 +1,5 @@
 const axios = require('axios').default
+const R = require('ramda')
 
 const client = axios.create({
   baseURL: 'https://api.unsplash.com',
@@ -6,6 +7,30 @@ const client = axios.create({
     Authorization: `Client-ID ${process.env.UNSPLASH_API_KEY}`
   }
 })
+
+const getTagsFromImage = R.pipe(
+  R.propOr([], 'tags'),
+  R.map(R.prop('title'))
+)
+
+const formatImage = (image) => {
+  return {
+    image_ID: image.id,
+    thumbnails: image.urls.thumb,
+    previews: image.urls.regular,
+    title: image.alt_description,
+    source: 'Unsplash',
+    tags: getTagsFromImage(image)
+  }
+}
+
+const formatRes = R.pipe(
+  R.pathOr([], [
+    'data',
+    'results'
+  ]),
+  R.map(formatImage)
+)
 
 const searchImages = async (key) => {
   try {
@@ -15,7 +40,7 @@ const searchImages = async (key) => {
       }
     })
 
-    return res.data
+    return formatRes(res)
   } catch (error) {
     console.error(error);
     return []
