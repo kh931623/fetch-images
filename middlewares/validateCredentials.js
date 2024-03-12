19060b53a16fae7489840acae589a29482aa1f9c
Simon <kh931623@hotmail.com>
2024-03-12 10:48:44 +0800

    feat: trim credentials input by users

--- middlewares/validateCredentials.js
+++ middlewares/validateCredentials.js
@@ -1,3 +1,5 @@
+const R = require('ramda')
+
 const validateCredentials = (req, res, next) => {
   const {
     username,
@@ -6,6 +8,11 @@ const validateCredentials = (req, res, next) => {
 
   if (!username || !password) {
     return res.status(400).send('username and password are required!')
+  }
+
+  req.body = {
+    username: R.trim(username),
+    password: R.trim(password)
   }
 
   next()
